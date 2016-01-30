f8e5577472618ba9eecd8662f36ea6c7397780af
Tom Leonardsson <tom.leo@hotmail.com>
2016-01-30 10:08:56 +0100

    Started working on projectile

--- core/src/com/snittarna/gameScence/Projectile.java
+++ core/src/com/snittarna/gameScence/Projectile.java
@@ -1,5 +1,27 @@
 package com.snittarna.gameScence;
 
-public class Projectile {
+import com.badlogic.gdx.math.Vector2;
+import com.snittarna.framework.Animation;
+import com.snittarna.framework.GameObject;
 
+public class Projectile extends GameObject {
+	private int damage;
+	
+	private float angle;
+	private float speeed;
+	
+	public boolean explosive;
+	public boolean enemy;
+	
+	public Projectile(Vector2 position, Animation sprite) {
+		super(position, sprite);
+	}
+	
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+	}
+	
+	public int getDamage() {
+		return this.damage;
+	}
 }
